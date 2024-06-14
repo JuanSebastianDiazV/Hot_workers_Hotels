ea62b49b468af2af7736b879e089e4d6caf571b0
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-06-14 10:40:24 -0500

    feat(entry-form): Cusmotize entry-form

--- src/app/pages/entry-form/entry-form.page.ts
+++ src/app/pages/entry-form/entry-form.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController, ModalController } from '@ionic/angular';
 import { ButtonDataService } from '../../services/button-data/button-data.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-entry-form',
@@ -9,19 +9,33 @@ import { ButtonDataService } from '../../services/button-data/button-data.servic
   styleUrls: ['./entry-form.page.scss'],
 })
 export class EntryFormPage implements OnInit {
-
+  loginForm!: FormGroup;
   constructor(
     private menuCtrl: MenuController,
     private modalCtrl: ModalController,
-    private buttonDataService: ButtonDataService
-  ) { }
+    private buttonDataService: ButtonDataService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.menuCtrl.enable(false);
+    this.formEntryForm();
+  }
+
+  formEntryForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
   getClickedButton() {
     return this.buttonDataService.getClickedButton();
   }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
