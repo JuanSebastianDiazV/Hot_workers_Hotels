456045beaed354f79a7b13b22809331512e05cc9
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-06-16 15:35:05 -0500

    feat(reset-password): add method navigate, desgin otp

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -9,13 +9,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ResetPasswordPage implements OnInit {
   emailForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder,
+    private router: Router
+  ) { 
     this.emailForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
     });
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.router.navigate(['/otp-reset-password']);
   }
 
 }
