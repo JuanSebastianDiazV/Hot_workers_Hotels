df186bacdf6ad01f7884904b6fc1a118d225b14d
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-06-17 01:29:28 -0500

    feat(otp-reset-password): create page

--- src/app/pages/otp-reset-password/otp-reset-password.page.ts
+++ src/app/pages/otp-reset-password/otp-reset-password.page.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from '../../services/alerts/alerts.service';
 
 @Component({
   selector: 'app-otp-reset-password',
   templateUrl: './otp-reset-password.page.html',
   styleUrls: ['./otp-reset-password.page.scss'],
 })
-export class OtpResetPasswordPage implements OnInit {
+export class OtpResetPasswordPage {
+  otp: string = '';
+  otpArray: string[] = new Array(6).fill('');
 
-  constructor() { }
+  numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-  ngOnInit() {
+  constructor(private router: Router, private alertService: AlertService) {}
+
+  addDigit(digit: number) {
+    if (this.otp.length < 6) {
+      this.otp += digit.toString();
+      this.updateOtpArray();
+    }
   }
 
+  removeDigit() {
+    if (this.otp.length > 0) {
+      this.otp = this.otp.slice(0, -1);
+      this.updateOtpArray();
+    }
+  }
+
+  updateOtpArray() {
+    this.otpArray = this.otp.split('');
+    while (this.otpArray.length < 6) {
+      this.otpArray.push('');
+    }
+  }
+
+  requestNewCode() {
+    // Lógica para solicitar un nuevo código
+  }
+
+  continue() {
+    if (this.otp.length === 6) {
+      // Navegar a una nueva ruta
+      this.router.navigate(['/new-route']);
+    } else {
+      // Mostrar mensaje de error o alert
+      this.alertService.presentErrorAlert(
+        'Advertencia',
+        'Por favor, ingresa un código de 6 dígitos.'
+      );
+    }
+  }
 }
