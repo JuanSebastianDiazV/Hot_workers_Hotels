c323150d1caf6b346fea2c56d18c131c99a1f2a6
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-07-06 10:42:37 -0500

    feat(balance):create layout

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
-
+  notifications = [
+    {
+      message: 'Andres quiere agendar una cita contigo',
+      linkText: 'Ir a la gestión de reservas',
+    },
+    {
+      message: 'Pepe canceló tu reserva',
+      linkText: 'Ir a la gestión de reservas',
+    },
+    {
+      message: 'Pepe canceló tu reserva',
+      linkText: 'Ir a la gestión de reservas',
+    },
+    {
+      message: 'Mau canceló tu reserva',
+      linkText: 'Ir a la gestión de reservas',
+    },
+    {
+      message: 'Pepe quiere agendar una cita contigo',
+      linkText: 'Ir a la gestión de reservas',
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
