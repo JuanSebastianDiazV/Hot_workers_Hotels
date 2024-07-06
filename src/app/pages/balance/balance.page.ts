670badc64981db21a60222181d7c0e97c953b721
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-07-06 10:29:54 -0500

    feat(balance-page)create layout

--- src/app/pages/balance/balance.page.ts
+++ src/app/pages/balance/balance.page.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balance.page.scss'],
 })
 export class BalancePage implements OnInit {
+  view: string = 'earnings'; // Vista actual, puede ser 'earnings' o 'paymentHistory'
+
+  earnings = [
+    {
+      name: 'Pepe',
+      date: 'Domingo, 16 febrero',
+      time: '09:00 a.m.',
+      location: 'Hotel Las Margaritas',
+      amount: '$ 100.000'
+    },
+    {
+      name: 'Ana',
+      date: 'Lunes, 17 febrero',
+      time: '10:00 a.m.',
+      location: 'Hotel Las Margaritas',
+      amount: '$ 150.000'
+    }
+  ];
+
+  paymentHistory = [
+    {
+      name: 'Juan',
+      date: 'Lunes, 17 febrero',
+      time: '10:00 a.m.',
+      location: 'Hotel Los Andes',
+      amount: '$ 200.000'
+    },
+    {
+      name: 'Carlos',
+      date: 'Martes, 18 febrero',
+      time: '11:00 a.m.',
+      location: 'Hotel Los Pinos',
+      amount: '$ 150.000'
+    }
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  showEarnings() {
+    this.view = 'earnings';
   }
 
+  showPaymentHistory() {
+    this.view = 'paymentHistory';
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,9 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
- 
+export class HomePage implements OnInit {
+  location: { lat: number; lon: number } | null = null;
 
-  constructor() { }
-
+  constructor() {}
+  ngOnInit(): void {
+    this.location = { lat: 40.7128, lon: -74.006 };
+  }
 }
