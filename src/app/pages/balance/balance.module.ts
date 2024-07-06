d126c9af10312e7e0136c404c081ef11c645de53
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-07-06 08:21:14 -0500

    feat(pages):centralized styles

--- src/app/pages/balance/balance.module.ts
+++ src/app/pages/balance/balance.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { BalancePageRoutingModule } from './balance-routing.module';
-
 import { BalancePage } from './balance.page';
+import { ComponentsModule } from '../../components/components.module'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BalancePageRoutingModule
+    BalancePageRoutingModule,
+    ComponentsModule
   ],
   declarations: [BalancePage]
 })
--- src/app/pages/rating/rating.module.ts
+++ src/app/pages/rating/rating.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RatingPageRoutingModule } from './rating-routing.module';
-
 import { RatingPage } from './rating.page';
+import { ComponentsModule } from '../../components/components.module'
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RatingPageRoutingModule
+    RatingPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [RatingPage]
 })
