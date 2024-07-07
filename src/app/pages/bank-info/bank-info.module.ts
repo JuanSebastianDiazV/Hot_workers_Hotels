0b76866b05cda7b9e5472753dbe75957931c94c9
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-07-07 09:11:24 -0500

    feat(bank-info): create layout

--- src/app/pages/bank-info/bank-info.module.ts
+++ src/app/pages/bank-info/bank-info.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { BankInfoPageRoutingModule } from './bank-info-routing.module';
-
 import { BankInfoPage } from './bank-info.page';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ComponentsModule } from '../../components/components.module'
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BankInfoPageRoutingModule
+    BankInfoPageRoutingModule,
+    ComponentsModule,
+    ReactiveFormsModule
   ],
   declarations: [BankInfoPage]
 })
--- src/app/pages/bank-info/bank-info.page.ts
+++ src/app/pages/bank-info/bank-info.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from '../../services/alerts/alerts.service';
+import { ProfileProgressService } from '../../services/profile-progress-service/profile-progress-service.service';
 
 @Component({
   selector: 'app-bank-info',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bank-info.page.scss'],
 })
 export class BankInfoPage implements OnInit {
+  bankInfoForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private progressService: ProfileProgressService,
+    private alertService: AlertService  // Asegúrate de que este es el nombre correcto del servicio de alertas
+  ) {}
 
   ngOnInit() {
+    this.bankInfoForm = this.formBuilder.group({
+      bank: ['', Validators.required],
+      accountType: ['', Validators.required],
+      accountNumber: ['', Validators.required]
+    });
   }
 
+  save() {
+    if (this.bankInfoForm.valid) {
+      // Lógica para guardar los datos
+      this.progressService.updateProgress('bank-info');
+      
+      // Mostrar la alerta de confirmación usando el servicio de alertas
+      this.alertService.presentWarningAlert(
+        '¡Éxito!',
+        'Información Bancaria Actualizada Con Éxito',
+        () => {
+          this.router.navigate(['/profile']);
+        }
+      );
+    }
+  }
 }
