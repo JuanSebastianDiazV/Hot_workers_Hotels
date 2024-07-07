b2dce22e5810428c57403faa3218abc06f760198
Juan Sebastian Diaz <verajuansebastian1@gmail.com>
2024-07-07 12:37:03 -0500

    feat(capacitor.config.ts): change appName

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'hot',
+  appName: 'hot workers & hotels',
   webDir: 'www',
   server: {
     androidScheme: 'https'
